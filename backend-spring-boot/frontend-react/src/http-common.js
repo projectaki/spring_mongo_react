64d08796a74867680dee354988478b36e3181c58
projectaki <akosegypro@gmail.com>
2021-03-10 09:36:05 +0100

    fixed file paths

--- backend-spring-boot/frontend-react/src/http-common.js
+++ backend-spring-boot/frontend-react/src/http-common.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export default axios.create({
-  baseURL: "http://localhost:8080/api/todo",
+  baseURL: "http://spring-mongo-react.herokuapp.com/api/todo",
   headers: {
     "Content-type": "application/json"
   }
